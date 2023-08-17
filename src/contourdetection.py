148544f1a722dbde187b9fb593b27036bb90be86
K-tokikawa <tokikawa@alte.co.jp>
2023-08-17 11:44:19 +0900

    透過できたけど、背景と同じ色の物体の色も透過される

--- src/contourdetection.py
+++ src/contourdetection.py
@@ -8,7 +8,7 @@ clipping = glob.glob(clippingpath)
 index = 0
 for pathcl in clipping:
     index = index + 1
-    if index == 1:
+    if True:
 
         img = cv2.imread(pathcl)
 
@@ -26,12 +26,17 @@ for pathcl in clipping:
 
         gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
         ret, thresh = cv2.threshold(gray, 70, 255, cv2.THRESH_BINARY)
-        ret, thresh = cv2.threshold(gray, 70, 255, cv2.THRESH_BINARY)
-        thresh = 255 - thresh
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # thresh = 255 - thresh
+        mask = np.all(img[:,:,:] == [255, 255, 255], axis=-1)
+        dst = cv2.cvtColor(thresh, cv2.COLOR_BGR2BGRA)
+        dst[mask,3] = 0
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+        img = cv2.add(dst, img)
+        cv2.imwrite("dst.png", dst)
 
         # 輪郭を画像に書き込む
         output = cv2.drawContours(img, contours, -1, (0,255,0), 3)
-
-        cv2.imshow('window', output)
-        cv2.waitKey(0)
+        cv2.imwrite(f'.\\clear\\img_{index}.png', img)
+        # cv2.imshow('window', img)
+        # cv2.waitKey(0)
